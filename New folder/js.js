f2763b0577f91fa875c0dc99c07d805abda5ff05
yanyan428 <yyan41291@gmail.com>
2025-12-07 11:57:57 -0500

    Add files via upload

---
+++ New folder/js.js
@@ -0,0 +1,46 @@
+var x=1;
+var y=2;
+var z=3;
+
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+var x =20;
+
+console.log(x);
+
+// just use let
+
+
+let x=1;
+let y=2;
+let z=3;
+
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+let x =20;
+
+console.log(x);
+
+
+
+function init() {
+    let x=20;
+    console.log(x);
+
+}
+
+init();
+
+
+
+
+document.getElementById("p1").innerHTML="HI"
+
+
+window.addEventListener("DOMContentLoaded").init();
